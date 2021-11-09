115fdec575b51ea8d0dcc1b26855158e31f60d38
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-11-09 16:08:37 -0500

    commit tree 29ecd0c2f224a8e3d004902241ce895436c89f80

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,49 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :discount, :last_item
 
+    @@all = []
+
+    def initialize(input = 0)
+        @total = 0
+        @discount = input
+    end
+
+    def add_item title, price, quantity = 1
+        self.total += price * quantity
+        quantity.times {@@all << title}
+        @last_item = price * quantity
+    end
+
+    def apply_discount
+        self.clear_cart
+        if discount != 0
+            @total = @total.to_f * (1.0 - @discount.to_f / 100.0)
+            "After the discount, the total comes to $#{@total.to_i}."
+        else
+            'There is no discount to apply.'
+        end
+    end
+
+    def items
+        @@all
+    end
+
+    def void_last_transaction
+        if @total > 0
+            @total -= @last_item
+        else
+            nil
+        end
+        @total.to_f
+        # if @total = 0
+        #     0.0
+        # else
+        #     nil
+        # end
+    end
+
+    def clear_cart
+        @@all = []
+    end
+end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -78,6 +78,7 @@ describe 'CashRegister' do
     context 'when called with three arguments' do
       it 'returns an array containing all items that have been added, including multiples' do
         new_register = CashRegister.new
+        new_register.clear_cart
         new_register.add_item("eggs", 1.99, 2)
         new_register.add_item("tomato", 1.76, 3)
         expect(new_register.items).to eq(["eggs", "eggs", "tomato", "tomato", "tomato"])
